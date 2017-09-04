3f8a6e9d6d839747258f9c44beea136260cd9ab7
shanying27 <13520192263@163.com>
2017-09-04 23:04:10 +0800

    打包
    第二次提交

--- SpringBootDemo/src/main/java/com/dc/cn/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/dc/cn/SpringBootDemoApplication.java
@@ -1,14 +1,17 @@
 package com.dc.cn;
 
+import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication{
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
+
 }
